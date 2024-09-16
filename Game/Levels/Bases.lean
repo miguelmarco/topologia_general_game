5c8336795d31bc387f578cd9eeab66ed6fe664d3
mmarco <mmarco@kali>
2024-09-16 20:40:35 +0200

    Añadir nivel de bases

---
+++ Game/Levels/Bases.lean
@@ -0,0 +1,11 @@
+import Game.Levels.Bases.CaracterizacionBases
+World "Bases"
+Title "Bases"
+
+Introduction "
+Pasemos ahora al mundo de las bases de abiertos.
+
+En un espacio topológico, una base es una familia formada por abiertos,
+pero que son suficientes para generar los demás abiertos mediante uniones
+arbitrarias.
+"
