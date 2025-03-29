2fee9e3fb6d0634a7e59c35e36967f16b62ea442
mmarco <mmarco@kali>
2025-03-29 19:53:20 +0100

    Add compacidad level

---
+++ Game/Levels/Compacidad.lean
@@ -0,0 +1,21 @@
+import Game.Levels.Compacidad.CompactoT2
+Title "Compacidad"
+
+Introduction "
+En este mundo veremos una noción importante
+en la topología: la noción de **compacidad**.
+
+Dado un subconjunto $A$, una familia de subconjuntos $ℱ$
+se dice que es un *recubrimiento* de $A$, si $A ⊆ \\bigcup ℱ$.
+Una subfamilia de $ℱ$ que también sea un recubrimiento de $A$,
+se dice que es un *subrecubrimiento* de $ℱ$. Si estamos
+en un espacio topológico, un recubrimiento
+formado por abiertos se dice que es un *recubrimiento abierto*.
+
+Un conjunto $A$ de un espacio topológico $X$ se dice que
+es *compacto* si todo recubrimiento abierto de $A$ admite
+un subrecubrimiento finito.
+
+El espacio $X$ se dirá a su vez que es un espacio compacto
+si el propio conjunto total es compacto.
+"
